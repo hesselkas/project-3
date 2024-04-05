f1f2bb5e2034f33d9f382343deb11a1cdbbb3b69
Red$ <9023330@student.zadkine.nl>
2024-04-05 13:01:12 +0200

    4 gyikugmhuk gfndyuk e  uibl

---
+++ over_ons.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+<div class="poopy">
+  <nav id="navbar">
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+  </nav>
+</div>
+
+</body>
+
+
