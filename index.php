eae558eeb89108c302fefe0f7870f25fa4f19840
jakupnkc <jakinukic12@gmail.com>
2024-04-05 13:45:13 +0200

    v

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="poopy">
+    <nav id="navbar">
+
+        <img src="images-removebg-preview.png">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+<?php ?>
