4c34e7e289581eaffddd0913fc1c472cf8da78ad
jakupnkc <jakinukic12@gmail.com>
2024-04-08 10:52:26 +0200

    sj;nknojhfbkj; byto
    bilsgfnuipwbn

--- index.php
+++ index.php
@@ -7,20 +7,20 @@
 </head>
 <body>
 
-<div class="poopy">
-    <nav id="navbar">
+<nav class="navbar">
 
-        <img src="images-removebg-preview.png">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </nav>
-</div>
+    <ul class="nav-list">
+        <img class="logo" src="images-removebg-preview.png" >
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+    </div>
+</nav>
 
 
 </body>
