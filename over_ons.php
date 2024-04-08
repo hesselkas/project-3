3863318c7567487f1ee13f7a3141243c526429f7
Red$ <9023330@student.zadkine.nl>
2024-04-08 14:34:19 +0200

    fghgf

--- over_ons.php
+++ over_ons.php
@@ -4,21 +4,28 @@
 <head>
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
-
 <body>
-<div class="bar">
-  <nav id="navbar">
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-  </nav>
-</div>
+<div class="container"></div>
 
+<nav class="navbar">
+
+    <ul class="nav-list">
+        <img class="logo" src="images-removebg-preview.png" >
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+    </div>
+</nav>
+
+<img class="logo1" src="images-removebg-preview.png">
+<h1> </h1>
+<div class="footer">
+<h1>hdihaihsdhi</h1>
+</div>
 </body>
 
 
