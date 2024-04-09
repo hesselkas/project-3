c1ceac6f7ad125bd91b069c81a4231843f793f83
jakupnkc <jakinukic12@gmail.com>
2024-04-09 15:31:25 +0200

    awd
    homepage

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <body>
 
 <nav class="navbar">
-
+    <img class="logo" src="images-removebg-preview.png" >
     <ul class="nav-list">
-        <img class="logo" src="images-removebg-preview.png" >
+
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About Us</a></li>
         <li><a href="#services">Services</a></li>
@@ -19,8 +19,12 @@
         <li><a href="#services">Services</a></li>
         <li><a href="#contact">Contact</a></li>
     </ul>
-    </div>
+
 </nav>
+
+<button id="knopmain"> Leer ons kennen</button>
+
+<img class="mainlogo" src="images-removebg-preview.png" >
 
 
 </body>
