d8decf4a60c820a0012748d1c86b65d4fdb5e2dc
jakupnkc <jakinukic12@gmail.com>
2024-04-17 10:43:12 +0200

    ee
    poep

--- index.php
+++ index.php
@@ -25,13 +25,24 @@
 
 <h1 class="textmain"> Heeft u een reperatie nodig kom gerust lang.</h1>
 
-<button class="knopmain"><a href="over_ons.php"  </a> Leer ons kennen</button>
+<button class="knopmain"><a href="over_ons.php"></a> Leer ons kennen</button>
 
+
+<div>
 <img class="mainlogo" src="images-removebg-preview.png" >
+</div
 
+<div>
+    <p> de uneed-it webshop</p>
+    <h1> BEZOEK DE OFFICIELE WEBSHOP</h1>
+    <p>Op de Uneed-IT Webshop vindt u hoogwaardige producten met de service die u van ons gewend bent. Wij doen niet aan 'leveren en zelf uitzoeken', maar voelen ons verantwoordelijk voor elk product dat wij leveren.</p>
+    <p>Dat wil zeggen dat wij graag de extra stap zetten om zeker te weten dat u goed overweg kunt met het product en afgestemd is op uw situatie. Alle producten worden standaard geleverd met drie jaar garantie! Kortom, bij Uneed-IT kunt u met gerust hart uw nieuwe laptop of printer aanschaffen. Wij staan voor u klaar om u vrijblijvend advies te geven over onze producten.</p>
 
+</div>
+
+   <div>
 <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d39284.770344537326!2d4.670808!3d52.01966600000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5d17dc64b3607%3A0xc5236f0d48a8cb14!2sZuidbaan%20514%2C%202841%20MJ%20Moordrecht%2C%20Nederland!5e0!3m2!1snl!2sus!4v1713254843498!5m2!1snl!2sus"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+   </div>
 
 <div class="footer">
     <h3 id="textfooter"> Zuidbaan 514, 2841MD Moordrecht</h3>
